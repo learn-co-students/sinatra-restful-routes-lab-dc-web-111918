23f8d359726f6b1ae78bce9fbbb83dc31a684653
github username <github email address>
2018-12-12 21:37:47 -0500

    finished new.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+  	@recipes = Recipe.all
+  	erb :index
+  end
+
+  get '/recipes/new' do
+  	@recipes = Recipe.new
+  	erb :new
+  end
+
+  post '/recipes' do
+  	name = params[:name]
+  	ingredients = params[:ingredients]
+  	cook_time = params[:cook_time]
+  	@recipe = Recipe.create(name: name, ingredients: ingredients, cook_time: cook_time)
+    # binding.pry
+    redirect "/recipes/#{@recipe.id}"
+  end
+
 
 end
